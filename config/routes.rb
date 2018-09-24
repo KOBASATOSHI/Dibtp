f2700c7a70cd7591a6699b7e75bacb0fdb62deba
yot0201412 <laughing.my.way0201@gmail.com>
2018-09-24 08:00:15 +0000

    add User table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'task_masters#index'
-  resources :task_masters
+  resources :task_masters, :users
 end
