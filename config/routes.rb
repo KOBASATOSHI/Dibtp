d23d429e155816ea4109f1378e6f06fcb10bc1bf
satoshikonishi <s.k.september.24@gmail.com>
2018-12-23 07:44:49 +0000

    login_complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { :omniauth_callbacks => "omniauth_callbacks" }
   root 'pages#index'
   get 'pages/show'
+
+  devise_scope :user do
+  get :sign_in, to: 'devise/sessions#new', as: :new_user_session
+  delete :sign_out, to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  resources :to_do_groups, shallow: true do
