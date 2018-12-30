c07166ac93424f6f6f99554a823c2326d9a06fda
yot0201412 <laughing.my.way0201@gmail.com>
2018-12-30 07:40:04 +0000

    add progresbar pages#show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,6 +15,7 @@ class PagesController < ApplicationController
       @to_dos = current_user.to_dos.on_going
       @fin_to_dos = current_user.to_dos.fin
       @fin_to_dos_count = @fin_to_dos.where("count= ? and start_count = ?", 1, @user_to_do_group_start_count).count
+      @fin_rate = @to_do_masters_count > 0 ? (@fin_to_dos_count.to_f / @to_do_masters_count.to_f).round(3) * 100 : 0
     end
   end
 end
