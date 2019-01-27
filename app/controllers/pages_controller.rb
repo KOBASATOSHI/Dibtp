48c7fc996b153c471f6c925e3868a1d4b1ad09df
satoshikonishi <s.k.september.24@gmail.com>
2019-01-27 08:21:16 +0000

    complete login system

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,20 +8,20 @@ class PagesController < ApplicationController
   end
 
   def show
-    @to_do_group = current_user.current_active_to_do_group
-    @to_dos = current_user.to_dos.on_going
-    @fin_to_dos = current_user.to_dos.fin
-    if !@to_do_group.nil?
-      @to_do_masters_count = @to_do_group.to_do_masters.count
-      @user_to_do_group_start_count = current_user.user_to_do_groups.active[0].start_count
-      @to_dos = current_user.to_dos.on_going
+    # @to_do_group = current_user.current_active_to_do_group
+    # @to_dos = current_user.to_dos.on_going
+    # @fin_to_dos = current_user.to_dos.fin
+    # if !@to_do_group.nil?
+    #   @to_do_masters_count = @to_do_group.to_do_masters.count
+    #   @user_to_do_group_start_count = current_user.user_to_do_groups.active[0].start_count
+    #   @to_dos = current_user.to_dos.on_going
       
-      # 現在ActiveなToDoGroupのToDoだけ取得する
-      @to_do_masters_ids = @to_do_group.to_do_master_ids
-      @fin_to_dos = current_user.to_dos.where(to_do_master_id: @to_do_masters_ids).fin
+    #   # 現在ActiveなToDoGroupのToDoだけ取得する
+    #   @to_do_masters_ids = @to_do_group.to_do_master_ids
+    #   @fin_to_dos = current_user.to_dos.where(to_do_master_id: @to_do_masters_ids).fin
       
-      @fin_to_dos_count = @fin_to_dos.where("count= ? and start_count = ?", 1, @user_to_do_group_start_count).count
-      @fin_rate = @to_do_masters_count > 0 ? (@fin_to_dos_count.to_f / @to_do_masters_count.to_f).round(3) * 100 : 0
-    end
+    #   @fin_to_dos_count = @fin_to_dos.where("count= ? and start_count = ?", 1, @user_to_do_group_start_count).count
+    #   @fin_rate = @to_do_masters_count > 0 ? (@fin_to_dos_count.to_f / @to_do_masters_count.to_f).round(3) * 100 : 0
+    # end
   end
 end
